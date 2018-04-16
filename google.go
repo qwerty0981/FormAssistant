765ccb8034fd26d60f078a2786f8881471448928
qwerty0981 <qwerty.0981@gmail.com>
2018-04-16 19:01:04 -0400

    Added Post function

--- google.go
+++ google.go
@@ -1,5 +1,12 @@
 package form
 
+import (
+   "errors"
+   "net/url"
+   "net/http"
+   "strings"
+)
+
 type googleForm struct{
    url string
    endpoints map[string]string
@@ -10,6 +17,27 @@ func GoogleForm(initialUrl string) googleForm{
    return f
 }
 
-func (gf googleForm) addEndpoint(name, endpoint string) {
+func (gf googleForm) AddEndpoint(name, endpoint string) {
    gf.endpoints[name] = endpoint
+}
+
+func (gf googleForm) Post(values map[string]string) error {
+   for key, _ := range values {
+      _, ok := gf.endpoints[key]
+      if ok == false {
+         return errors.New("Key (" + key + ") not found in endpoints")
+      }
+   }
+
+   httpClient := http.Client{}
+
+   form := url.Values{}
+   for k, v := range values {
+      form.Add(gf.endpoints[k], v)
+   }
+
+   req, _ := http.NewRequest("POST", gf.url, strings.NewReader(form.Encode()))
+   req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+   httpClient.Do(req)
+   return nil
 }
